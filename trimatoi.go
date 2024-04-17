582bbed1058002b9e27d33a17d4e8dcc2718e31b
moboutrig <165959484+moboutrig@users.noreply.github.com>
2024-04-17 09:12:36 +0100

    Update trimatoi.go
    my TrimAtoi

--- trimatoi.go
+++ trimatoi.go
@@ -1,28 +1,17 @@
-package piscine
+package piscine 
 
 func TrimAtoi(s string) int {
-	numbers, isNegative := parseStringToRune(s)
-	n := Atoi(string(numbers))
-	if isNegative {
-		return -n
-	}
-	return n
-}
-
-func parseStringToRune(s string) ([]rune, bool) {
-	numbers := []rune{}
-	isNegative := false
-	firstDigit := false
-	for i := 0; i < len(s); i++ {
-		if !firstDigit {
-			if s[i] == '-' {
-				isNegative = true
-			}
-		}
-		if IsNumeric(string(s[i])) {
-			numbers = append(numbers, rune(s[i]))
-			firstDigit = true
+	runes := []rune(s)
+	sign := 1
+	result := 0
+	foundDigit := false
+	for _, r := range runes {
+		if unicode.IsDigit(r) {
+			foundDigit = true
+			result = result*10 + int(r-'0')
+		} else if r == '-' && !foundDigit {
+			sign = -1
 		}
 	}
-	return numbers, isNegative
+	return result * sign
 }
